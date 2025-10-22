c2f09a68a76d946815c58c587a2d31650d6e82fc
Aozaki <i@aozaki.cc>
2025-10-22 21:42:05 +0800

    fix: add missing nextTs config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,11 @@
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
 
 import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
 const eslintConfig = [
   ...nextVitals,
+  ...nextTs,
   prettierConfigRecommended,
   {
     ignores: ['**/node_modules/**', '.next/**', 'dist/**', 'build/**', 'coverage/**', 'out/**'],
