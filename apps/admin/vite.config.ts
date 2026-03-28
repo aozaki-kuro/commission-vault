de6cde186aadc88de03a4ad141b4d3b2ff8d96e7
Aozaki <i@aozaki.cc>
2026-03-28 16:59:32 +0800

    chore: use vite 8

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules/react-dom') || id.includes('node_modules/react/') || id.includes('node_modules/scheduler')) {
